ce7153243550fb4218dba25e1319fa552fb10a55
Mike Ligthart <m.e.u.ligthart@vu.nl>
2025-02-04 17:06:56 +0100

    adds alpamini action demo

--- demos/alphamini/demo_alphamini_google_tts.py
+++ demos/alphamini/demo_alphamini_google_tts.py
@@ -4,7 +4,7 @@ from sic_framework.core.message_python2 import AudioMessage, AudioRequest
 from sic_framework.devices.common_desktop.desktop_speakers import SpeakersConf
 from sic_framework.devices.desktop import Desktop
 from sic_framework.services.text2speech.text2speech_service import Text2Speech, Text2SpeechConf, GetSpeechRequest, SpeechResult
-from sic_framework.devices.mini import MiniRobot
+from sic_framework.devices.minirobot import MiniRobot
 from sic_framework.devices.common_mini.mini_speaker import MiniSpeakersConf
 
 
@@ -20,5 +20,5 @@ save the file in conf/dialogflow/google_tts_keyfile.json
 tts_conf = Text2SpeechConf(keyfile=abspath(join('..', '..', 'conf', 'dialogflow', 'google_tts_keyfile.json')))
 tts = Text2Speech(conf=tts_conf)
 reply = tts.request(GetSpeechRequest(text="Hallo, Ik ben een alphamini", voice_name="nl-NL-Standard-G", ssml_gender="NEUTRAL"))
-mini = MiniRobot(ip="192.168.178.111", speaker_conf=MiniSpeakersConf(sample_rate=reply.sample_rate))
+mini = MiniRobot(speaker_conf=MiniSpeakersConf(sample_rate=reply.sample_rate))
 mini.speaker.request(AudioRequest(reply.waveform, reply.sample_rate))
