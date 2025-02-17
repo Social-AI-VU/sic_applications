4d77204878286472a7efee939b222dcfa63db978
Mike Ligthart <m.e.u.ligthart@vu.nl>
2025-02-17 15:28:38 +0100

    clean demo for the alphamini tts

--- demos/alphamini/demo_alphamini_google_tts.py
+++ demos/alphamini/demo_alphamini_google_tts.py
@@ -1,16 +1,13 @@
 from os.path import abspath, join
 
 from sic_framework.core.message_python2 import AudioMessage, AudioRequest
-from sic_framework.devices.common_desktop.desktop_speakers import SpeakersConf
-from sic_framework.devices.desktop import Desktop
 from sic_framework.services.text2speech.text2speech_service import Text2Speech, Text2SpeechConf, GetSpeechRequest, SpeechResult
 from sic_framework.devices.alphamini import Alphamini
 from sic_framework.devices.common_mini.mini_speaker import MiniSpeakersConf
 
 
 """
-Google Text2Speech service should be running. You can start it with:
-[services/text2speech] python text2speech_service.py 
+Google Text2Speech service should be running. You can start it with: run-google-tts
 
 NOTE: you need to have setup Cloud Text-to-Speech API in your Google Cloud Console and configure the credential keyfile.
 See https://console.cloud.google.com/apis/api/texttospeech.googleapis.com/ 
@@ -19,6 +16,6 @@ save the file in conf/dialogflow/google_tts_keyfile.json
 
 tts_conf = Text2SpeechConf(keyfile=abspath(join('..', '..', 'conf', 'dialogflow', 'google_tts_keyfile.json')))
 tts = Text2Speech(conf=tts_conf)
-reply = tts.request(GetSpeechRequest(text="Hallo! Ik ben de droomrobot. Ik ga jou helpen hier in het ziekenhuis.", voice_name="nl-NL-Standard-D", ssml_gender="FEMALE"))
-mini = Alphamini(ip="10.0.0.123", speaker_conf=MiniSpeakersConf(sample_rate=reply.sample_rate))
+reply = tts.request(GetSpeechRequest(text="Hi, I am an alphamini", voice_name="en-US-Standard-C", ssml_gender="FEMALE"))
+mini = Alphamini(ip="10.0.0.xxx", mini_id="00xxx", mini_password="alphago", redis_ip="10.0.0.yyy", speaker_conf=MiniSpeakersConf(sample_rate=reply.sample_rate))
 mini.speaker.request(AudioRequest(reply.waveform, reply.sample_rate))
