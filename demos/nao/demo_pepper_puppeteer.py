a76f6f013efd4441f3bdda7a62404a3afdef81be
Ting-Chia Chiang <t.chiang@vu.nl>
2025-05-01 19:27:14 +0200

    Update the pepper puppeteer to make it more robust

--- demos/nao/demo_pepper_puppeteer.py
+++ demos/nao/demo_pepper_puppeteer.py
@@ -1,11 +1,6 @@
 import time
 
 from sic_framework.devices import Pepper
-from sic_framework.devices.common_naoqi.nao_motion_streamer import (
-    NaoMotionStreamerConf,
-    StartStreaming,
-    StopStreaming,
-)
 from sic_framework.devices.common_naoqi.naoqi_autonomous import (
     NaoBackgroundMovingRequest,
     NaoBasicAwarenessRequest,
@@ -22,6 +17,11 @@ from sic_framework.devices.common_naoqi.naoqi_stiffness import Stiffness
 from sic_framework.devices.common_naoqi.naoqi_text_to_speech import (
     NaoqiTextToSpeechRequest,
 )
+from sic_framework.devices.common_naoqi.pepper_motion_streamer import (
+    PepperMotionStreamerConf,
+    StartStreaming,
+    StopStreaming,
+)
 
 JOINTS = ["Head", "RArm", "LArm"]
 # FIXED_JOINTS = ["RLeg", "LLeg"]
@@ -30,7 +30,10 @@ JOINTS = ["Head", "RArm", "LArm"]
 class PuppeteerApplication:
     def __init__(self, master_ip="10.0.0.165", puppet_ip="10.0.0.196"):
 
-        self.conf = NaoMotionStreamerConf(samples_per_second=30)
+        self.conf = PepperMotionStreamerConf(samples_per_second=30)
+        # set the stiffness of the puppet to 1 to control the joints fully, default is 0.6
+        self.puppet_conf = PepperMotionStreamerConf(stiffness=1)
+
         # self.puppet_master = Pepper(
         #     master_ip,
         #     motion_stream_conf=self.conf,
@@ -38,15 +41,16 @@ class PuppeteerApplication:
         #     test_repo="/home/karen/social-interaction-cloud",
         # )
         self.puppet_master = Pepper(
-            master_ip, motion_stream_conf=self.conf, dev_test=True
+            master_ip, pepper_motion_conf=self.conf, dev_test=True
         )
-
         # self.puppet = Pepper(
         #     puppet_ip,
         #     dev_test=True,
         #     test_repo="/home/karen/social-interaction-cloud",
         # )
-        self.puppet = Pepper(puppet_ip, dev_test=True)
+        self.puppet = Pepper(
+            puppet_ip, pepper_motion_conf=self.puppet_conf, dev_test=True
+        )
 
         self._setup_robots()
 
@@ -62,10 +66,15 @@ class PuppeteerApplication:
         # Completely disable all autonomous capabilities (including obstacle avoidance) on the master Pepper to prevent interference
         # all the stiffness of its motors will be off, so it will slouch down
         self.puppet_master.autonomous.request(NaoSetAutonomousLifeRequest("disabled"))
+        # turn off the basic awareness of the puppet, so it doesn't move its head around (but it seems like its still moving)
+        # See more details here: http://doc.aldebaran.com/2-5/ref/life/autonomous_abilities_management.html#autonomous-abilities-management
+        # to find out if it's possible to enable minimal Autonomous Life without disabling obstacle avoidance functionality
+        self.puppet.autonomous.request(NaoBasicAwarenessRequest(False))
 
         print(
             "----------------------------------Waking up robots----------------------------------"
         )
+
         # wake up robots (Important: for pepper, the stiffness can't be set when it is in rest mode, unlike nao)
         self.puppet_master.autonomous.request(NaoWakeUpRequest())
         self.puppet.autonomous.request(NaoWakeUpRequest())
@@ -74,17 +83,10 @@ class PuppeteerApplication:
             "----------------------------------Setting stiffness of robots----------------------------------"
         )
 
-        # First, turn off the smart stiffness to avoid interference and make the movement of the master pepper easier
-        self.puppet_master.motion.request(NaoqiSmartStiffnessRequest(False))
-        self.puppet.motion.request(NaoqiSmartStiffnessRequest(False))
-
         # On Pepper, stiffness somehow can't be set at the individual joint level, so we just pass the chains instead of the joints
         self.puppet_master.stiffness.request(
             Stiffness(0.0, joints=JOINTS, enable_joint_list_generation=False)
         )
-
-        # Set the stiffness of the puppet to 1.0 to control the joints fully
-        self.puppet.stiffness.request(Stiffness(1, joints=JOINTS))
 
     def on_touch(self, message):
         if self.is_paused:
@@ -118,12 +120,14 @@ class PuppeteerApplication:
 
         self.puppet_master.motion_streaming.request(StopStreaming())
 
+        # enable the autonomous life of the master pepper before going to rest
+        self.puppet_master.autonomous.request(NaoSetAutonomousLifeRequest("solitary"))
+
         # Set both robots in rest pose again
-        self.puppet_master.motion.request(NaoqiSmartStiffnessRequest(True))
         self.puppet_master.autonomous.request(NaoRestRequest())
         self.puppet.autonomous.request(NaoRestRequest())
-        # self.puppet.stiffness.request(Stiffness(0, joints=JOINTS))
-        # self.puppet_master.stiffness.request(Stiffness(0, joints=JOINTS))
+        # use it when you don't want to make peppers rest after every experiment
+        # self.puppet.stiffness.request(Stiffness(0, joints=JOINTS, enable_joint_list_generation=False))
 
 
 if __name__ == "__main__":
