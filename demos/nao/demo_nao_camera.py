185da6147d725deb4a2870d30a2bfafb633a7ed1
landnn <landonnbrownn@gmail.com>
2025-07-11 15:06:01 +0200

    made Nao MU camera demo the standard

--- demos/nao/demo_nao_camera.py
+++ demos/nao/demo_nao_camera.py
@@ -17,9 +17,13 @@ def on_image(image_message: CompressedImageMessage):
 # See "NaoqiCameraConf" for more options like brightness, contrast, sharpness, etc
 conf = NaoqiCameraConf(vflip=1)
 
-nao = Nao(ip="192.168.0.0", top_camera_conf=conf)
+print("Initializing Nao...")
+nao = Nao(ip="10.0.0.198", top_camera_conf=conf, dev_test=True, test_repo="/Users/apple/Desktop/SAIL/SIC_Development/social-interaction-cloud")
+
+print("Registering callback...")
 nao.top_camera.register_callback(on_image)
 
+print("Starting demo...")
 while True:
     img = imgs.get()
     cv2.imshow("", img[..., ::-1])  # cv2 is BGR instead of RGB
